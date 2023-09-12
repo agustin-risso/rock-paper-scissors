1a7d7760f080d082ea6d13f4d186cb92bf1d29c9
Agustín Risso <agustinrisso89@gmail.com>
2023-09-11 21:11:18 -0300

    Creation of Array and getComputerChoice function

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ let playerChoice;
 let playerChoiceLower;
 let playerSelection;
 let computerSelection;
+
+const compArray = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+	return compArray[~~(Math.random() * compArray.length)];
+}
